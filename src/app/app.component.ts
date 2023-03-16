a95fdefe85fac85131acba20dab5f9aadeed6a23
MURILO NUNES DOS SANTOS <49132949+MuriloNSantos17@users.noreply.github.com>
2023-03-16 20:15:16 -0300

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Ecommerce Full Stack - Murilo Nunes dos Santos Unyleya';
+  title = 'Ecommerce - Full Stack';
 }
